9dc8c4b3efdb791352e3f60b11e0b2e1d9886034
sere06001 <sere06001@utb.vaxjo.se>
2024-02-05 23:57:05 +0100

    Add project files.

---
+++ Uppgift7/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+namespace uppgift7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] strings1 = { "a", "b", "b", "c", "hej" };
+            string[] strings2 = { "hej", "a", "a", "b", "programmering" };
+            foreach (string gemensamtOrd in FinnsIBåda(strings1, strings2))
+            {
+                Console.WriteLine(gemensamtOrd);
+            }
+            Console.ReadKey();
+        }
+        static List<string> FinnsIBåda(string[] a, string[] b)
+        {
+            List<string> list = new List<string>();
+            foreach (string s in a)
+            {
+                foreach (string s2 in b)
+                {
+                    if (s == s2 && !list.Contains(s))
+                    {
+                        list.Add(s2);
+                    }
+                }
+            }
+            return list;
+        }
+    }
+}
